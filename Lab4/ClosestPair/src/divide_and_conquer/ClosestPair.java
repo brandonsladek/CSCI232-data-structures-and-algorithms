7984ec154f8a906e5f2815c01e15980b8a927983
brandonsladek <brandonsladek@gmail.com>
2015-04-27 14:33:03 -0600

    Fixed closestSplitPair method and combining results printout

--- Lab4/ClosestPair/src/divide_and_conquer/ClosestPair.java
+++ Lab4/ClosestPair/src/divide_and_conquer/ClosestPair.java
@@ -60,43 +60,44 @@ public class ClosestPair {
 		
 	} // End Pair
 	
-	// Declare new arraylist for point objects
-	static ArrayList<Point> points = new ArrayList<Point>();
+	// Declare new arraylist for point objects sorted by x-value
+	static ArrayList<Point> pointsX = new ArrayList<Point>();
+	
 	
 	// Main method
 	public static void main(String[] args) {
 				
-		// Hardcode points into arraylist
-		points.add(new Point(2.0, 7.0));
-		points.add(new Point(4.0, 13.0));
-		points.add(new Point(5.0, 8.0));
-		points.add(new Point(10.0, 5.0));
-		points.add(new Point(14.0, 9.0));
-		points.add(new Point(15.0, 5.0));
-		points.add(new Point(17.0, 7.0));
-		points.add(new Point(19.0, 10.0));
-		points.add(new Point(22.0, 7.0));
-		points.add(new Point(25.0, 10.0));
-		points.add(new Point(29.0, 14.0));
-		points.add(new Point(30.0, 2.0));
+		// Hardcode points into x-sorted arraylist
+		pointsX.add(new Point(2.0, 7.0));
+		pointsX.add(new Point(4.0, 13.0));
+		pointsX.add(new Point(5.0, 8.0));
+		pointsX.add(new Point(10.0, 5.0));
+		pointsX.add(new Point(14.0, 9.0));
+		pointsX.add(new Point(15.0, 5.0));
+		pointsX.add(new Point(17.0, 7.0));
+		pointsX.add(new Point(19.0, 10.0));
+		pointsX.add(new Point(22.0, 7.0));
+		pointsX.add(new Point(25.0, 10.0));
+		pointsX.add(new Point(29.0, 14.0));
+		pointsX.add(new Point(30.0, 2.0));
 		
 		
 		// Sort the points by x coordinate
-		Collections.sort(points, new Comparator<Point>() {
+		Collections.sort(pointsX, new Comparator<Point>() {
 			public int compare(Point one, Point two) {
 				return Double.compare(one.getX(), two.getX());
 			}
 		});
 		
 		// Print input points
-		printInputPoints( points );
+		printInputPoints( pointsX );
 		
-		System.out.println("----------------------------------------");
+		System.out.println("-------------------------------------------------------------------");
 		
 		// Get closest pair of points
-		Pair closest = closestPair( points );
+		Pair closest = closestPair( pointsX );
 		
-		System.out.println("---------------------------------------------------------");
+		System.out.println("-------------------------------------------------------------------");
 		
 		// Print final result
 		System.out.println("Final result: P1: ("+closest.p1.getX()+","+closest.p1.getY()+"), ("
@@ -112,8 +113,8 @@ public class ClosestPair {
 		
 		// Print each input pair
 		for(int i = 0; i < points.size(); i++) {
-			// Print newline after each triple
-			if ((i+1)%3 == 0) {
+			// Print newline after six points
+			if ((i+1)%6 == 0) {
 			System.out.println("(" + points.get(i).getX() + "," + points.get(i).getY() + ")");
 			} else {
 				System.out.print("(" + points.get(i).getX() + "," + points.get(i).getY() + ")");
@@ -134,8 +135,8 @@ public class ClosestPair {
 	public static Pair closestPair(ArrayList<Point> subset) {
 
 		// Get subset indices with respect to original points arraylist
-		int start = points.indexOf(subset.get(0));
-		int finish = points.indexOf(subset.get((subset.size()-1)));
+		int start = pointsX.indexOf(subset.get(0));
+		int finish = pointsX.indexOf(subset.get((subset.size()-1)));
 		
 		System.out.println("Solving problem: Point[" + start + "]...Point[" + finish + "]");
 		
@@ -185,10 +186,10 @@ public class ClosestPair {
 		Pair d3 = closestSplitPair(subset.get(median).getX(), subset, Math.min(d1.distance, d2.distance));
 		
 		// Get indices for two subsets (with respect to original points arraylist)
-		int index1 = points.indexOf(firstHalf.get(0));
-		int index2 = points.indexOf(firstHalf.get(firstHalf.size()-1));
-		int index3 = points.indexOf(secondHalf.get(0));
-		int index4 = points.indexOf(secondHalf.get(secondHalf.size()-1));
+		int index1 = pointsX.indexOf(firstHalf.get(0));
+		int index2 = pointsX.indexOf(firstHalf.get(firstHalf.size()-1));
+		int index3 = pointsX.indexOf(secondHalf.get(0));
+		int index4 = pointsX.indexOf(secondHalf.get(secondHalf.size()-1));
 		
 		// Print conquer status
 		System.out.println("Combining Problems: Point["+index1+"]...Point["+index2+"] and Point["+index3+"]...Point["+index4+"]");
@@ -229,15 +230,6 @@ public class ClosestPair {
 	
 	// Method to find closest pair across split
 	public static Pair closestSplitPair(double split, ArrayList<Point> points, double d) {
-		
-		//double minimalDistance = Integer.MAX_VALUE;
-		
-		// Sort the points by y coordinate
-		Collections.sort(points, new Comparator<Point>() {
-			public int compare(Point one, Point two) {
-				return Double.compare(one.getY(), two.getY());
-			}
-		});
 				
 		// Declare arraylist for points within d range of split
 		ArrayList<Point> pointsWithinRange = new ArrayList<Point>();
@@ -252,8 +244,8 @@ public class ClosestPair {
 		// Create new closestSplitPair (not updated)
 		Pair closestSplitPair = new Pair(null, null, Integer.MAX_VALUE);
 		
-		// The inner loop only runs six times because the difference in the y-values of the points 
-		// must be less than the current closest pair distance if the distance is to be computed
+		// The inner loop only runs at most six times because the difference in the y-values of the points 
+		// must be less than the closest pair distance of the two subsets if the distance is to be computed
 		for (int i = 0; i < pointsWithinRange.size(); i++) {
 			for (int j = i+1; j < pointsWithinRange.size() && pointsWithinRange.get(j).getY() - pointsWithinRange.get(i).getY() < d; j++) {
 				if (distance(pointsWithinRange.get(i), pointsWithinRange.get(j)) < d) {
@@ -264,50 +256,7 @@ public class ClosestPair {
 					closestSplitPair.p2 = pointsWithinRange.get(j);
 				}
 			}
-		}
-		
-// Might try it this way...
-		
-// At most six new comparisons across split
-		
-//		double d1 = Integer.MAX_VALUE;
-//		double d2 = Integer.MAX_VALUE;
-//		double d3 = Integer.MAX_VALUE;
-//		double d4 = Integer.MAX_VALUE;
-//		double d5 = Integer.MAX_VALUE;
-//		double d6 = Integer.MAX_VALUE;
-		
-//			// If point is in left subsplit
-//			if (pointsWithinRange.get(i).getX() < split) {
-//				
-//			// Lowest point
-//				if (i == 0) {
-//				d1 = distance(pointsWithinRange.get(i), pointsWithinRange.get(i+1));
-//					d2 = distance(pointsWithinRange.get(i), pointsWithinRange.get(i+2));
-//					d3 = distance(pointsWithinRange.get(i), pointsWithinRange.get(i+3));
-//				}
-//				// Second lowest point
-//				if (i == 1) {
-//					
-//				}
-//				// Third lowest point
-//				if (i == 2) {
-//					
-//				}
-//				// Highest point
-//				if (i == pointsWithinRange.size()) {
-//					
-//				}
-//				// Second highest point
-//				if (i == pointsWithinRange.size()-1) {
-//					
-//				}
-//				// Third highest point
-//				if (i == pointsWithinRange.size()-2) {
-//					
-//				}
-//			}
-		
+		}		
 		return closestSplitPair;
 	} // End closestSplitPair
 	
